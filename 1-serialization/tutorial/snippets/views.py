bd970084da9ab80b2f2398b8de105f1969b0abb5
xjuggl3r <xjuggl3r@gmail.com>
2023-02-01 22:24:46 -0400

    More polishing with generic class-based views

--- 1-serialization/tutorial/snippets/views.py
+++ 1-serialization/tutorial/snippets/views.py
@@ -1,37 +1,21 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status, mixins, generics
+from rest_framework import generics
 
 # Create your views here.
 
+# REST framework provides a set of already mixed-in generic views that we can use to trim down our views.py module even more.
 
-# rewriting the root view using mixins
-class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+
+#refactoring the views using generic class-based views
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-# rewriting the detail view using mixins
-class SnippetDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin, generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+# The core functionality is provided by the mixin classes, and we're simply adding the .ListCreateAPIView and .RetrieveUpdateDestroyAPIView classes to provide the actions that we want to support.
